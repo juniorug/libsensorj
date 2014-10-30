dd6d8f7fe5283946a8fdccb34dddd09d096d4548
Júnior <juniorug@gmail.com>
2014-10-30 01:30:18 -0300

    cleaning code

--- src/main/java/com/source/concreteSensor/DHT11Humidity.java
+++ src/main/java/com/source/concreteSensor/DHT11Humidity.java
@@ -5,6 +5,8 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import com.source.interfaces.ISensor;
 
 public class DHT11Humidity implements ISensor {
@@ -19,6 +21,17 @@ public class DHT11Humidity implements ISensor {
         final GpioPinDigitalInput dht11Hum = gpio.provisionDigitalInputPin(
                 RaspiPin.GPIO_04, PinPullResistance.PULL_DOWN);
 
+        // create and register gpio pin listener
+        dht11Hum.addListener(new GpioPinListenerDigital() {
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(
+                    GpioPinDigitalStateChangeEvent event) {
+                // display pin state on console
+                System.out.println(" --> GPIO PIN STATE CHANGE: "
+                        + event.getPin() + " = " + event.getState());
+            }
+        });
+        
         // return instance;
     }
 
